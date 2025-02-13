a523d166511dd931cad85c4f21870d40149a8b1a
Ramon Alves <ramon.alves160@gmail.com>
2025-02-13 15:34:10 -0300

    Décimo terceiro Commit - Feat: Criação do index.js e da validação do email.

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form')
+const emailInput = document.querySelector('input#email')
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const errorSpan = document.querySelector('span.error')
+
+
+// Functions to Add or Remove the "error border" to input
+function addError() {
+    emailInput.classList.add('invalid')
+}
+function removeError() {
+    emailInput.classList.remove('invalid')
+}
+
+
+// Function to test email values as valid or not
+function verifyEmail() {
+
+    if (emailInput.value == '') 
+    {
+        addError()
+        errorSpan.innerText = 'Email cannot be empty'
+    }
+    else if (!emailRegex.test(emailInput.value))
+    {
+        addError()
+        errorSpan.innerText = 'Please provide a valid email address'
+    }
+    else
+    {
+        removeError()
+        errorSpan.innerText = ''
+    }
+}
+
+
+// Form verification
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    verifyEmail()
+}) 
